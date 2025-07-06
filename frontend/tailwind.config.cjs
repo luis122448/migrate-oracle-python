b7e6050759366906f97d6df212990047b4b32240
luis122448 <luis122448@gmail.com>
2025-07-06 13:24:46 -0500

    feat: add tailwind config

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+};
