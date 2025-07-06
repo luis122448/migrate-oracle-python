6a80d9a757c35482fb42cfbfa280e9c91af5bd1d
luis122448 <luis122448@gmail.com>
2025-07-06 15:47:57 -0500

    fix: add migration logs button

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // TODO: Add your domain here
   base: '/', // Add this line for correct base path
   integrations: [sitemap(), tailwindcss()],
   vite: {
